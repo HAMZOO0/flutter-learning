e2b60473602908e5947f758b3af98ea2e310cd4d
Hamza <hamzakhan174722@gmail.com>
2025-02-26 22:09:30 +0500

    Initial commit

---
+++ firstapp/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, // Remove debug banner
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My Name App"),
+          backgroundColor: Colors.blue, // Change color if needed
+        ),
+        body: Center(
+          child: Text(
+            "Hamza  ",
+
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
+  }
+}
